98b13be0e474ffb1b6a4f84b3e9caa91d100296a
Mike Tang <daogangtang@gmail.com>
2018-11-20 18:51:17 +0800

    update.
    Signed-off-by: Mike Tang <daogangtang@gmail.com>

--- src/tcp_server.rs
+++ src/tcp_server.rs
@@ -1,3 +1,6 @@
+use futures::future::Future;
+use futures::sink::Sink;
+use futures::stream::Stream;
 
 use tokio::io;
 use tokio::net::TcpListener;
@@ -11,30 +14,39 @@ use tokio::codec::Decoder;
 use std::time::{Duration, Instant};
 use tokio::timer::Delay;
 
-
 fn main() {
-    
-    let addr = env::args().nth(1).unwrap_or("127.0.0.1:8080".to_string());
+    let addr = env::args()
+        .nth(1)
+        .unwrap_or("127.0.0.1:8080".to_string());
     let addr = addr.parse::<SocketAddr>().unwrap();
 
     let socket = TcpListener::bind(&addr).unwrap();
     println!("Listening on: {}", addr);
 
-    let task = socket.incoming()
+    let task = socket
+        .incoming()
         .map_err(|e| println!("failed to accept socket; error = {:?}", e))
         .for_each(move |socket| {
-            println!("start delay!");
+            let framed = BytesCodec::new().framed(socket);
+            let (sink, stream) = framed.split();
 
-            let when = Instant::now() + Duration::from_millis(1000);
-            let a_task = Delay::new(when)
-                .map_err(|e| println!("{:?}", e))
-                .then(|_| {
-                    io::write_all(socket, b"welcome, guy!")
-                })
-                .then(|_| Ok(()));
+            let a_stream = stream.and_then(|bytes| {
+                println!("bytes: {:?}", bytes);
+                println!("start delay!");
+                Delay::new(Instant::now() + Duration::from_millis(1000))
+                    .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, "oh no!"))
+                    .and_then(|_|{
+                        Ok("welcome, guy!".into())
+                    })
+            });
 
-            tokio::spawn(a_task)
+            let a = sink.send_all(a_stream)
+                    .map(|_| ())
+                    .map_err(|e| println!("==> {:?}", e));
+
+            tokio::spawn(a)
         });
 
     tokio::run(task);
 }
+
